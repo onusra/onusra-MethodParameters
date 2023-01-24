03eb75fe7ad58f69960bc2542499d577a3ed1d07
onusra <onusra@angelo.edu>
2023-01-24 17:46:38 +0000

    Completed Parameters Lab

--- src/main/java/Parrot.java
+++ src/main/java/Parrot.java
@@ -25,7 +25,10 @@ public class Parrot {
      * @param text is a parameter passed into this method by whatever called it.
      * @return text.
      */
+    public static void main(String[] args){
+
+    }
     public String copy(String text){
-        return "return text please!";
+        return text;
     }
 }
